b61c1f97351c6a5bd3783e2721bf82e4d09397b8
Arthur Villar <=>
2021-02-25 12:46:05 -0500

    Finish main page

--- resources/views/principal/stats.blade.php
+++ resources/views/principal/stats.blade.php
@@ -1,16 +1,16 @@
 <section class="container mb-7">
 	<div class="row">
-		<div class="col-lg-4 col-md-4 col-12 px-5">
+		<div class="col-lg-4 col-md-4 col-12 px-5 mb-2">
 			<h2 class="text-primary"><span class="counter" data-count="90" data-duration="3000">0</span>%</h2>
 			<h5>dos empregados afirmam que a remuneração não é a únicaforma de motivação</h5>
 			<p class="m-0 text-muted">(Gallup, 2017)</p>
 		</div>
-		<div class="col-lg-4 col-md-4 col-12 px-5" style="border-right: 1px grey dashed; border-left: 1px grey dashed">
+		<div class="col-lg-4 col-md-4 col-12 px-5 mb-2" style="border-right: 1px grey dashed; border-left: 1px grey dashed">
 			<h2 class="text-primary"><span class="counter" data-count="15" data-duration="2000">0</span>%</h2>
 			<h5>dos funcionários estão ativamente engajados com otrabalho</h5>
 			<p class="m-0 text-muted">(Gallup, 2017)</p>
 		</div>
-		<div class="col-lg-4 col-md-4 col-12 px-5">
+		<div class="col-lg-4 col-md-4 col-12 px-5 mb-2">
 			<h2 class="text-primary"><span class="counter" data-count="85" data-duration="2900">0</span>%</h2>
 			<h5>estão não engajados (psicologicamente desconectado, diasperdidos) ou desengajados</h5>
 			<p class="m-0 text-muted">(Gallup, 2017)</p>
